04b1bf3efdb39102b47adae39f020278a06998c4
1Coder-Shivam <6868shivamkumarmaurya@gmail.com>
2024-04-26 15:57:05 +0530

    Project setup commit

---
+++ src/test/java/com/skm/scm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skm.scm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
